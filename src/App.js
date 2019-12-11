d547f764af1fad17562714e6508861505751758b
jinah <jinah0818@gmail.com>
2019-12-11 18:59:44 +0900

    2_Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}) {
+return <div>
+    <h2>I like {name}</h2>
+    <img src = {picture} />
+</div>
+}
+
+
+const foodlike = [
+  {
+    name : "kimchi",
+    image : 
+    "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg"
+  },
+  {
+    name : "ramen",
+    image :
+    "http://static.hubzum.zumst.com/hubzum/2019/04/19/13/694050ce163a4655bc5032ac11e0bcd5.jpg"
+  },
+  {
+    name : "dduckbockgi",
+    image :
+    "http://t1.daumcdn.net/liveboard/diningcode/f4706d9efe7440afb665280ac9ca475e.JPG"
+  }
+]
+
+function renderFood() {
+  
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return  <div>
+      {foodlike.map(dish => (
+        <Food name={dish.name} picture = {dish.image} />
+      ))}
     </div>
-  );
 }
 
 export default App;
