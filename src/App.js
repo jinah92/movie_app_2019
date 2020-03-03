6a0e52639ef0052010f33bce3d1fdd82922e2d31
jinah <jinah0818@gmail.com>
2020-03-03 23:55:01 +0900

    Update movie list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react"
+import axios from "axios"
+import Movie from "./Movie"
 
 class App extends React.Component {
-  state={
+  state = {
     isLoading: true,
-    movies:[]
+    movies: []
   }
-  getMovies=async()=>{
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
-    console.log(movies);
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false })
   }
-  componentDidMount(){
-    this.getMovies();
-    
+  componentDidMount() {
+    this.getMovies()
   }
-  render(){
-    const { isLoading }=this.state;
-    return <div>{this.state.isLoading ? "Loading" : "we are ready"}</div>;
+  render() {
+    const { isLoading, movies } = this.state
+    return (
+      <div>
+        {this.state.isLoading
+          ? "Loading"
+          : movies.map(movie => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              )
+            })}
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
